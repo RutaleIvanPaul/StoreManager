42dad9d33267938e997e154dda27ce908a730ece
Seryazi-Andela <phillip.seryazi@gmail.com>
2019-11-06 16:05:16 +0300

    adding retrofit

--- app/src/main/java/com/kotlin/ivanpaulrutale/storemanager/models/ListItems.kt
+++ app/src/main/java/com/kotlin/ivanpaulrutale/storemanager/models/ListItems.kt
@@ -1,11 +1,44 @@
 package com.kotlin.ivanpaulrutale.storemanager.models
 
-class StoreItem(val art_number:String,val color:String, val description:String, val quantity:String, val store:String,val last_updated:String)
+import com.google.gson.annotations.SerializedName
 
-class ReportItem(val art_number:String,val color:String, val description:String, val quantity:String, val store:String,val checkout_time:String,val collector:String)
+class RequestResponse(
+    @SerializedName("success")
+    val success: Boolean,
+    @SerializedName("message")
+    val message: String,
+    @SerializedName("foundItems")
+    val storeItems: List<StoreItem>
+)
 
-class SummarisedReportItem(val art_number:String,val color:String, val description:String, var total_quantity:String)
+class StoreItem(
+    var id: Int = 0,
+    var artNumber: String = "",
+    var color: String = "",
+    var description: String = "",
+    var quantity: Int = 0,
+    var store: String = "",
+    var createdAt: String = "",
+    var updatedAt: String = ""
+)
 
-class SummarisedArtNoReportItem(val art_number:String,var total_quantity:String)
+class ReportItem(
+    val artNumber: String,
+    val color: String,
+    val description: String,
+    val quantity: String,
+    val store: String,
+    val checkoutTime: String,
+    val collector: String
+)
 
-class MonthListItem(val name:String)
+class SummarisedReportItem(
+    val art_number: String,
+    val color: String,
+    val description: String,
+    var total_quantity: String
+)
+
+class SummarisedArtNoReportItem(val art_number: String, var total_quantity: String)
+
+class MonthListItem(val name: String)
